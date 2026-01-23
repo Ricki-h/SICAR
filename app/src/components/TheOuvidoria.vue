e75ca099b314ed21086f59b4bbb85d0e8e5b028b
Augusto Andrei de Melo Maux <augusto.maux@escolar.ifrn.edu.br>
2026-01-23 14:47:15 -0300

    Adicionando elementos de texto

--- app/src/components/TheOuvidoria.vue
+++ app/src/components/TheOuvidoria.vue
@@ -7,27 +7,27 @@ import { ref } from 'vue'
   <!-- OUVIDORIA -->
    <div class="flex flex-col gap-12 w-304 m-auto">
         <div class="flex flex-col items-center gap-3">
-            <h1 class="text-4xl text-orange-600 font-bold font-sans">OUVIDORIA</h1>
-            <p class="font-[Rubik]">Aqui te ouvimos independentemente do que tem a dizer</p>
+            <h1 class="text-[40px] text-orange-600 font-bold font-sans">OUVIDORIA</h1>
+            <p class="font-[Rubik] text-base">Aqui te ouvimos independentemente do que tem a dizer</p>
         </div>
 
         <div class="flex flex-row items-center justify-evenly">
             
             <div class="flex flex-col justify-center items-center gap-2 rounded-sm shadow-md bg-blue-50 w-70 h-70">
                 <img src="../assets/mega-phone.svg"></img>
-                <h1>DENÚNCIA</h1>
+                <h2 class="text-2xl font-semibold text-blue-950">DENÚNCIA</h2>
             </div>
             <div class="flex flex-col justify-center items-center gap-2 rounded-sm shadow-md bg-blue-50 w-70 h-70">
                 <img src="../assets/comment.svg"></img>
-                <h1>SOLICITAÇÃO</h1>
+                <h2 class="text-2xl font-semibold text-blue-950">SOLICITAÇÃO</h2>
             </div>
             <div class="flex flex-col justify-center items-center gap-2 rounded-sm shadow-md bg-blue-50 w-70 h-70">
                 <img src="../assets/Like.svg"></img>
-                <h1>ELOGIO</h1>
+                <h2 class="text-2xl font-semibold text-blue-950">ELOGIO</h2>
             </div>
             <div class="flex flex-col justify-center items-center gap-2 rounded-sm shadow-md bg-blue-50 w-70 h-70">
                 <img src="../assets/Dislike.svg"></img>
-                <h1>RECLAMAÇÕES</h1>
+                <h2 class="text-2xl font-semibold text-blue-950">RECLAMAÇÕES</h2>
             </div>
 
         </div>
