a75625c28a38ea6dd54b7734cabe37a342771452
Henrique <rick1n.htz@gmail.com>
2026-01-23 11:49:15 -0300

    fazendo componente de botão

---
+++ app/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+  import BaseButton from './components/BaseButton.vue'
+</script>
+
+<template>
+  <BaseButton color="neutral">ENTRAR</BaseButton>
+</template>
+
+<style scoped>
+
+</style>
