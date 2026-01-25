3d3173e3a76e82b47b01f1c29a3f2f3822c4569c
Hugo-505 <hugo.moura@escolar.ifrn.edu.br>
2026-01-25 12:20:08 -0300

    Colocando Serviços

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,64 +2,70 @@
 import CardOuvidoria from './components/CardOuvidoria.vue'
 import TheDropdown from './components/TheDropdown.vue'
 import TheOuvidoria from './components/TheOuvidoria.vue'
+import TheServico from './components/TheServico.vue';
+import CardServico from './components/CardServico.vue';
 </script>
 
 <template>
-  <div class="max-w-7xl mx-auto">
-    <div class="max-w-5xl mx-auto flex flex-col gap-10 max-sm:px-8 items-center">
-      <TheOuvidoria class="flex flex-col gap-3 md:items-center">
-          <template #title>
-            OUVIDORIA
-          </template>
+  <div class="container mx-auto">
+    <div class="container mx-auto flex flex-col gap-10 max-sm:px-8 items-center">
+      <div class="flex flex-col gap-3 items-center max-lg:self-start max-lg:pl-13 max-md:pl-1">
+        <TheServico class="flex flex-col items-center max-lg:items-start">
+            <template #title>
+              SERVIÇOS
+            </template>
 
-          <template #subtitle>
-            Aqui te ouvimos independentemente do que tem a dizer.
-          </template>
-      </TheOuvidoria>
-
-      <div class="p-0 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6 md:gap-8">
-
-        <CardOuvidoria>
-          <template #imagem>
-            <img src="./assets/mega-phone.svg"></img>
-          </template>
-
-          <template #title>
-            <h2 class="text-2xl font-semibold text-blue-950">DENÚNCIA</h2>
-          </template>
-        </CardOuvidoria>
-
-        <CardOuvidoria>
-          <template #imagem>
-            <img src="./assets/comment.svg"></img>
-          </template>
-
-          <template #title>
-            <h2 class="text-2xl font-semibold text-blue-950">SOLICITAÇÃO</h2>
-          </template>
-        </CardOuvidoria>
-
-        <CardOuvidoria>
-          <template #imagem>
-            <img src="./assets/Like.svg"></img>
-          </template>
-
-          <template #title>
-            <h2 class="text-2xl font-semibold text-blue-950">ELOGIO</h2>
-          </template>
-        </CardOuvidoria>
-
-        <CardOuvidoria>
-          <template #imagem>
-            <img src="./assets/Dislike.svg"></img>
-          </template>
-
-          <template #title>
-            <h2 class="text-2xl font-semibold text-blue-950">RECLAMAÇÕES</h2>
-          </template>
-        </CardOuvidoria>
-        
+            <template #subtitle>
+              Acesse os melhores serviços para você
+            </template>
+        </TheServico>
       </div>
+      
+      <div class="grid grid-cols-4 gap-8 max-lg:gap-6 max-sm:gap-8 max-lg:grid-cols-2 max-sm:grid-cols-1">
+        <CardServico category="SERVIÇO POPULAR"
+        title="CONSULTA DE SALDO"
+        description="Lorem impsum Lorem impsum dolor Lorem impsum Lorem impsum sit Lorem impsum Lorem impsum amet Lorem impsum Lorem impsum "
+         >
+          <template #icon>
+            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 46 32" fill="none">
+              <path d="M23.1826 0V4.64138H38.2101L26.4386 21.0585L16.643 11.0958C16.2152 10.6607 15.6349 10.4163 15.0299 10.4163C14.4248 10.4163 13.8446 10.6607 13.4167 11.0958L0 28.7185L3.22638 32L15.0299 16.018L24.8254 25.9807C25.2533 26.4158 25.8335 26.6602 26.4386 26.6602C27.0436 26.6602 27.6239 26.4158 28.0518 25.9807L41.4365 7.92284V23.2069H46V0H23.1826Z" fill="#14171B"/>
+            </svg>
+          </template>
+        </CardServico>
+        <CardServico category="SERVIÇO RECOMENDADO" 
+        title="CONSULTA DE SALDO"
+        description="Lorem impsum Lorem impsum dolor Lorem impsum Lorem impsum sit Lorem impsum Lorem impsum amet Lorem impsum Lorem impsum "
+         />
+        <CardServico category="SERVIÇO FAVORITADO" 
+        title="CONSULTA DE SALDO"
+        description="Lorem impsum Lorem impsum dolor Lorem impsum Lorem impsum sit Lorem impsum Lorem impsum amet Lorem impsum Lorem impsum "
+         >
+        <template #icon>
+              <svg xmlns="http://www.w3.org/2000/svg" width="23" height="32" viewBox="0 0 23 32" fill="none">
+                <path d="M20.7 2.28571V28.2857L12.535 24.1829L11.5 23.6686L10.465 24.1829L2.3 28.2857V2.28571H20.7ZM20.7 0H2.3C1.69 0 1.10499 0.240816 0.673654 0.66947C0.242321 1.09812 0 1.67951 0 2.28571V32L11.5 26.2857L23 32V2.28571C23 1.67951 22.7577 1.09812 22.3263 0.66947C21.895 0.240816 21.31 0 20.7 0Z" fill="#14171B"/>
+              </svg>
+        </template>
+        </CardServico>
+        <CardServico category="SERVIÇO POPULAR" 
+        title="CONSULTA DE SALDO"
+        description="Lorem impsum Lorem impsum dolor Lorem impsum Lorem impsum sit Lorem impsum Lorem impsum amet Lorem impsum Lorem impsum "
+        >
+          <template #icon>
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 46 32" fill="none">
+                <path d="M23.1826 0V4.64138H38.2101L26.4386 21.0585L16.643 11.0958C16.2152 10.6607 15.6349 10.4163 15.0299 10.4163C14.4248 10.4163 13.8446 10.6607 13.4167 11.0958L0 28.7185L3.22638 32L15.0299 16.018L24.8254 25.9807C25.2533 26.4158 25.8335 26.6602 26.4386 26.6602C27.0436 26.6602 27.6239 26.4158 28.0518 25.9807L41.4365 7.92284V23.2069H46V0H23.1826Z" fill="#14171B"/>
+              </svg>
+          </template>
+        </CardServico>
+      </div>
+      
+
+      
+
+      <button class="flex px-4 py-5 rounded-full justify-center items-center gap-2 bg-[#FD6900]">
+        <a href="#" class="text-2xl font-bold text-white">
+          TODOS OS SERVIÇOS
+        </a>
+      </button> 
     </div>
   </div>
 
---
+++ app/src/components/CardServico.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { defineProps, defineEmits, computed } from 'vue';
+const colorClasses = {
+    gray: "#6B818F"
+}
+const sizeClasses = {
+    // tam_70: 'w-full max-w-[280px] aspect-square',
+    tam_70: 'w-full max-w-[280px] aspect-square',
+    tam_tablet: 'w-full max-w-[321px] h-[280px]',
+    tam_cell: 'w-full max-w-[325px] h-[280px]',
+    sm: 'w-48 h-48',
+    md: 'w-64 h-64',
+    lg: 'w-72 h-72'
+}
+const classes = computed(() => [
+    'flex flex-col justify-center items-center gap-5 rounded-sm shadow-md mx-auto p-4',
+    colorClasses[props.color] ?? colorClasses.gray,
+    sizeClasses[props.size] ?? sizeClasses.tam_70,
+    'delay-150 duration-300 ease-in-out'
+]);
+defineProps({
+  category: { type: String, default: 'Categoria' },
+  
+  title: { type: String, default: 'TÍTULO DO SERVIÇO' },
+  description: { type: String, default: 'Lorem impsum Lorem impsum dolor Lorem impsum Lorem impsum sit.' },
+  buttonText: { type: String, default: 'Acessar Serviço' }
+});
+</script>
+<template>
+    <div class="bg-white border border-gray-100 rounded-lg p-6 shadow-sm w-[280px] max-lg:w-[323px] flex flex-col">
+        <div class="flex gap-4 items-center mb-6 h-[32px] ">
+            <div class="icon-wrapper">
+                <slot name="icon">
+                <!-- Ícone Padrão (aparece se você não passar nada) -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                    <circle cx="12" cy="7" r="4"></circle>
+                </svg>
+                </slot>
+            </div>
+            <span class="font-semibold text-xl ">
+                {{ category }}
+            </span>
+
+        </div>
+        <hr class="border-gray-200 mb-5">
+        <div class="flex flex-col gap-2 mb-5 ">
+            <h3>
+                {{ title }}
+            </h3>
+            <p class="text-[gray] ">
+                {{ description }}
+            </p>
+        </div>
+        <button class="w-full bg-blue-600 hover:bg-blue-700 text-white font-semibold py-3 px-4 transition-colors">
+            <a href="#" class="font-bold">
+                {{ buttonText }}
+            </a>
+        </button>
+
+    </div>
+</template>
